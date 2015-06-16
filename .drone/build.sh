4b8ac93c84a2131f3f750f6ee3cb5bec134e5de2
lwsdevops <smith.w.lance@gmail.com>
2015-06-16 17:52:10 -0600

    another try

--- .drone/build.sh
+++ .drone/build.sh
@@ -5,5 +5,5 @@ if echo "${DRONE_BRANCH}" | grep -i feature; then
   npm install
   npm test
 elif [ "${DRONE_BRANCH}" == "integration" ]; then
-  curl -sv "${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}" || exit 1
+  curl -sv "http://${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}" || exit 1
 fi
