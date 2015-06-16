bbd3fc3dd81d7adc9a5129fdccf21c0edb2dead3
lwsdevops <smith.w.lance@gmail.com>
2015-06-16 17:47:55 -0600

    test

--- .drone/build.sh
+++ .drone/build.sh
@@ -4,9 +4,6 @@ env
 if echo "${DRONE_BRANCH}" | grep -i feature; then
   npm install
   npm test
-  git fetch origin
-  git checkout integration
-  git request-pull origin/integration "${DRONE_BRANCH}"
 elif [ "${DRONE_BRANCH}" == "integration" ]; then
   curl -sv "${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}/health" || exit 1
 fi
