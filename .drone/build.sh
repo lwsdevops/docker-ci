0f48aa7a030666f79f2205777b1e5465f25eaee4
lwsdevops <smith.w.lance@gmail.com>
2015-06-16 17:26:47 -0600

    trying full workflow

--- .drone/build.sh
+++ .drone/build.sh
@@ -4,8 +4,12 @@ env
 if echo "${DRONE_BRANCH}" | grep -i feature; then
   npm install
   npm test
+  git checkout integration
+  git merge "${DRONE_BRANCH}" .
+  git push origin integration
 elif [ "${DRONE_BRANCH}" == "integration" ]; then
-  npm install
-  npm integration
-  curl -sv "${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}/health"
+  curl -sv "${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}/health" || exit 1
+  git checkout integration
+  git merge "${DRONE_BRANCH}" .
+  git push origin master 
 fi
