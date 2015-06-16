d7a3d1f2ba8380b6f12060902001f9604a28cc70
lwsdevops <smith.w.lance@gmail.com>
2015-06-16 17:02:47 -0600

    testing connection to es

--- .drone/build.sh
+++ .drone/build.sh
@@ -4,7 +4,8 @@ env
 if echo "${DRONE_BRANCH}" | grep -i feature; then
   npm install
   npm test
-  curl -sv "${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}"
 elif [ "${DRONE_BRANCH}" == "integration" ]; then
-  curl -sv "${ELASTICSEARCH_PORT}"
+  npm install
+  npm integration
+  curl -sv "${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}/health"
 fi
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 var   http = require('http')
     , os = require('os')
-    , reuqest = require('request')
-    , url = require('url');
+    , request = require('request')
+    , url = require('url')
+    , es_ip = process.env.ELASTICSEARCH_PORT_9200_TCP_ADDR
+    , es_port = process.env.ELASTICSEARCH_PORT_9200_TCP_PORT;
 
 exports.server = http.createServer( function(req, res) {
     console.log(req.headers);
-    var uri = url.parse(req.url).pathname
+    var uri = url.parse(req.url).pathname;
 
     if( req.method == "GET" && uri == "/" ) {
         res.writeHead(200, { 'Content-Type' : 'text/plain' });
         res.write('hostname:' + os.hostname() + '\n');
         res.end();
-    } else {
+    } else if ( req.method == "GET" && uri == "/es-health" ) {
+        request({ uri: 'http://'+es_ip+':'+es_porti+'/health', method: 'GET' }, function( error, resp, body ) {
+          if (!error) {
+            res.writeHead(200, { 'Content-Type' : 'text/plain' });
+            res.write(body + '\n')
+          }
+        });
+    }
+    else {
         res.writeHead( 405, { 'Content-Type' : 'text/plain' });
         res.write('Fail!');
         res.end('Method Not Allowed');
