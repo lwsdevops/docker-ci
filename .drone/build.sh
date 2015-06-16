d30e5465ee6401b7b355b18df8b39a9833c39c8d
lwsdevops <smith.w.lance@gmail.com>
2015-06-16 16:46:32 -0600

    testing es

--- .drone/build.sh
+++ .drone/build.sh
@@ -4,6 +4,7 @@ env
 if echo "${DRONE_BRANCH}" | grep -i feature; then
   npm install
   npm test
-elif [ "${DRONE_BRANCH}" == "integration" ]; then
   curl -sv "${ELASTICSEARCH_PORT}"i
+elif [ "${DRONE_BRANCH}" == "integration" ]; then
+  curl -sv "${ELASTICSEARCH_PORT}"
 fi
